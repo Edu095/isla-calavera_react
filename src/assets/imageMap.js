59f8131f144df0c528899b2673f0c05851f614c2
Edu <eduojerb@gmail.com>
2025-12-27 18:41:05 +0100

    Add image mapping for fortune cards and dice icons

---
+++ src/assets/imageMap.js
@@ -0,0 +1,25 @@
+// Mapeo de imágenes para las cartas de acción
+export const FORTUNE_IMAGES = {
+  'none': null,
+  'pirate': '/resources/Carta_Pirata.png',
+  'skull': '/resources/Carta_Calavera.png',
+  'skull2': '/resources/Carta_Calavera_Doble.png',
+  'diamond': '/resources/Carta_Diamante.png',
+  'gold': '/resources/Carta_Moneda.png',
+  'chest': '/resources/Carta_Botin.png',
+  'monkeyParrot': '/resources/Carta_MonoLoro.png',
+  'sorceress': '/resources/Carta_Hechicera.png',
+  'ship2': '/resources/Carta_BarcoPirata_Dos.png',
+  'ship3': '/resources/Carta_BarcoPirata_Tres.png',
+  'ship4': '/resources/Carta_BarcoPirata_Cuatro.png'
+};
+
+// Mapeo de imágenes para los iconos de dados
+export const DICE_IMAGES = {
+  'skull': '/resources/Dado_Calavera.png',
+  'gold': '/resources/Dado_Moneda.png',
+  'diamond': '/resources/Dado_Diamante.png',
+  'saber': '/resources/Dado_Sables.png',
+  'parrot': '/resources/Dado_Loro.png',
+  'monkey': '/resources/Dado_Mono.png'
+};
